e4c1c519250f801e5b395d668df3a17c659a4d63
Tom <tjpalmer@tjpalmer.com>
2020-01-22 07:26:35 -0700

    Load data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hi!');
+addEventListener('load', main);
+
+async function main() {
+  let files = {
+    issues: 'gh-issue-event.json',
+    pulls: 'gh-pull-request.json',
+    pushes: 'gh-push-event.json',
+    stars: 'gh-star-event.json',
+    licenses: 'github-licenses.json',
+  };
+  let data = Object.assign(
+    {},
+    ...await Promise.all(Object.keys(files).map(async key => {
+      let file = files[key];
+      console.log(key, file);
+      let content = await (await fetch(`./src/data/${file}`)).json();
+      return {key: content};
+    })),
+  );
+}
