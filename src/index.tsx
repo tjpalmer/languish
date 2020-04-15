9c935df0d14ed9c8f2855988542072643bf61855
Tom <tjpalmer@tjpalmer.com>
2020-04-14 18:51:04 -0700

    Add queueMicrotask polyfill from MDN

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,15 @@ import ReactDOM from "react-dom";
 import "styles/index.css";
 import App from "./components/App";
 
+// Polyfill from MDN.
+if (typeof window.queueMicrotask !== "function") {
+  window.queueMicrotask = function (callback) {
+    Promise.resolve()
+      .then(callback)
+      .catch(e => setTimeout(() => { throw e; }));
+  };
+}
+
 ReactDOM.render(
   <GlobalProvider>
     <App />
