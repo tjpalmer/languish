d9c48c338a4b4f4cc72f571277bb307ec0a8140a
Tom <tom@contextfree.info>
2021-02-06 05:58:29 -0700

    Use read line for speed

--- process/src/main.rs
+++ process/src/main.rs
@@ -5,11 +5,21 @@ use std::io::{self, BufRead};
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     println!("Hello, {}", args[1]);
-    let file = File::open(&args[1])?;
-    let lines = io::BufReader::new(file).lines();
-    for line_result in lines.skip(1).take(10000) {
-        let line = line_result?;
+    let mut file = io::BufReader::new(File::open(&args[1])?);
+    let mut line = String::new();
+    let mut line_count = 0;
+    // Skip the header line.
+    file.read_line(&mut line)?;
+    loop {
+        if file.read_line(&mut line)? == 0 {
+            break;
+        }
         process_line(&line);
+        line.clear();
+        line_count += 1;
+        if line_count >= 10000 {
+            break;
+        }
     }
     Ok(())
 }
