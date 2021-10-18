8c29156ee9d40c55db1cc83b1c2d54d22f0690e4
Tom <tom@contextfree.info>
2021-10-18 05:22:15 -0700

    Update to 2021Q3; exclude pushes from mean

--- src/components/Metric.tsx
+++ src/components/Metric.tsx
@@ -3,16 +3,21 @@ import { clx, objectEntries } from "helpers";
 import { Metrics } from "parsedData";
 import React from "react";
 
+type MetricItem = {
+  info?: string;
+  label: string;
+};
+
 const Metric = () => {
   const global = useGlobal();
 
   // map of available metrics to human readable strings
-  const items: { [k in keyof Metrics]: string } = {
-    issues: "Issues",
-    mean: "Mean Score",
-    pulls: "Pull Requests",
-    pushes: "Pushes",
-    stars: "Stars",
+  const items: { [k in keyof Metrics]: MetricItem } = {
+    issues: { label: "Issues" },
+    mean: { label: "Mean Score", info: "Excludes pushes because bots" },
+    pulls: { label: "Pull Requests" },
+    pushes: { label: "Pushes" },
+    stars: { label: "Stars" },
   };
 
   // map of available metrics to human readable strings
@@ -32,7 +37,7 @@ const Metric = () => {
           <span className="yLabelArrow"></span>{" "}
           <span className="yLabelText">
             {global.scale === "log" ? "Log " : ""}
-            {items[global.metric]}
+            {items[global.metric].label}
           </span>{" "}
           <span className="yLabelArrow"></span>
         </div>
@@ -49,8 +54,9 @@ const Metric = () => {
                   real,
                   global.metric === real && "active"
                 )}
+                title={display.info}
               >
-                {display}
+                {display.label}
               </li>
             ))}
           </ul>
