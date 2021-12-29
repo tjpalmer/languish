8cd9f5ad988b8726d21ff8029c612bc472b52948
tjpalmer <tom@contextfree.info>
2021-12-29 12:26:50 +0000

    Deploying to gh-pages from publish @ e5eaaab25fc9c2d2a5a5db5d439afdadc6723a3e 🚀

---
+++ precache-manifest.01e2e35bde2f32236e8610f128376899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0813319ece4a335aff4532424b67532",
+    "url": "/languish/index.html"
+  },
+  {
+    "revision": "ceac4497a407d71ff5fd",
+    "url": "/languish/static/css/main.ddc7f94f.chunk.css"
+  },
+  {
+    "revision": "7c7d1fbd3fed1846ce1c",
+    "url": "/languish/static/js/2.f487bbff.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/languish/static/js/2.f487bbff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceac4497a407d71ff5fd",
+    "url": "/languish/static/js/main.d819d244.chunk.js"
+  },
+  {
+    "revision": "65392350c00afa0ee3da",
+    "url": "/languish/static/js/runtime-main.70049b58.js"
+  },
+  {
+    "revision": "8eea8979fa335904c64a0b76129b7cb2",
+    "url": "/languish/static/media/icomoon.8eea8979.woff"
+  }
+]);
