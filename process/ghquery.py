720dcdf99bd5caeadb2e6c78aaec0c99a530efff
Tom <tom@contextfree.info>
2022-01-01 11:35:30 -0700

    Use pandas

--- process/ghquery.py
+++ process/ghquery.py
@@ -2,7 +2,7 @@ import argparse
 import collections
 import csv
 import gql
-# import pandas as pd
+import pandas as pd
 import pathlib as pth
 import typing as typ
 
@@ -43,13 +43,29 @@ def run(*, args: Args):
     print(args)
     assert not pth.Path(args["output"]).exists()
     # Count up events by repo.
-    repo_totals = collections.defaultdict(lambda: 0)
-    for event_path in args["events"]:
-        print(event_path)
-        for repo, count in load_repo_counts(path=event_path).items():
-            repo_totals[repo] += count
-        print(f"total repos: {len(repo_totals)}")
-    repo_totals_sorted = sorted(repo_totals.items(), key=lambda pair: -pair[1])
+    # repo_totals = collections.defaultdict(lambda: 0)
+    totals = None
+    for events_path in args["events"]:
+        print(events_path)
+        counts = pd.read_csv(events_path)
+        print(f"events: {len(counts)}")
+        counts = sum_counts(counts)
+        print(f"repos: {len(counts)}")
+        if totals is None:
+            totals = counts
+        else:
+            totals = sum_counts(pd.concat([totals, counts]))
+        # print(event_path)
+        # for repo, count in load_repo_counts(path=event_path).items():
+        #     repo_totals[repo] += count
+        print(f"total repos: {len(totals)}")
+    # repo_totals_sorted = sorted(repo_totals.items(), key=lambda pair: -pair[1])
+    totals.sort_values(by="count", ascending=False, inplace=True)
+    totals.to_csv(args["output"], index=False)
+
+
+def sum_counts(counts: pd.DataFrame) -> pd.DataFrame:
+    return counts.groupby("repo")["count"].sum().to_frame().reset_index()
 
 
 if __name__ == "__main__":
